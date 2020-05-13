17f57008b1a547c372c1495e98f0636a16046968
Kim Young Nam <yn782@naver.com>
2020-05-13 17:21:51 +0900

    20.05.13 17:00

--- workspace/A_SSAFY_Study/Study_05/Uncomplete/AMain_2931_가스관.java
+++ workspace/A_SSAFY_Study/Study_05/Uncomplete/AMain_2931_가스관.java
@@ -50,7 +50,6 @@ public class AMain_2931_가스관 {
 				} else if (map[i][j] == 'M') {
 					sx = i;
 					sy = j;
-					q.add(new Pipe(i, j, -1, 'M'));// 처음은 +로 처리
 				}
 			}
 		}
@@ -59,40 +58,56 @@ public class AMain_2931_가스관 {
 	}
 
 	private static void solve() {
-		while (!q.isEmpty()) {
-			int size = q.size();
-			for (int i = 0; i < size; i++) {
-				Pipe p = q.poll();
-				if (map[p.x][p.y] == 'Z') {
-					res = p.del;
-					return;
-				}
-				for (int d = 0; d < 4; d++) {
-					if (!canGo(p, d)) // 현위치에서 갈수 있는 방향만 가기
-						continue;
-					int ix = p.x + dx[d];
-					int jy = p.y + dy[d];
-					if (!safe(ix, jy)) // 범위 밖인지 여부
-						continue;
-					if (map[ix][jy] == '.') {
-						if (p.del == null) {
-							for (int k = 0; k < 7; k++) {
-								if (pipeTF[k][d] == 0) {// 받아짐
-									char sha = ' ';
-									
-//									Pipe addp = new Pipe(ix,jy,d,);
-//									q.add(addp);
-								}
-							}
-						} else {
-							continue;
-						}
-					}
-
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (map[i][j] == '.') {
+					if (check4way(i, j))
+						return;
 				}
 			}
 		}
 	}
+
+	private static boolean check4way(int x, int y) {
+		
+		return false;
+	}
+
+//	private static void solve() {
+//		while (!q.isEmpty()) {
+//			int size = q.size();
+//			for (int i = 0; i < size; i++) {
+//				Pipe p = q.poll();
+//				if (map[p.x][p.y] == 'Z') {
+//					res = p.del;
+//					return;
+//				}
+//				for (int d = 0; d < 4; d++) {
+//					if (!canGo(p, d)) // 현위치에서 갈수 있는 방향만 가기
+//						continue;
+//					int ix = p.x + dx[d];
+//					int jy = p.y + dy[d];
+//					if (!safe(ix, jy)) // 범위 밖인지 여부
+//						continue;
+//					if (map[ix][jy] == '.') {
+//						if (p.del == null) {
+//							for (int k = 0; k < 7; k++) {
+//								if (pipeTF[k][d] == 0) {// 받아짐
+//									char sha = ' ';
+//									
+//									Pipe addp = new Pipe(ix,jy,d,);
+//									q.add(addp);
+//								}
+//							}
+//						} else {
+//							continue;
+//						}
+//					}
+//
+//				}
+//			}
+//		}
+//	}
 
 	private static boolean canGo(Pipe p, int d) {
 		char c = map[p.x][p.y];
