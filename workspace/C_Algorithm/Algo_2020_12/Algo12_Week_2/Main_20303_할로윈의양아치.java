fc63476b1103bebf1db7e42ffdd59f2fd830c084
Kim Young Nam <yn782@naver.com>
2020-12-10 17:08:46 +0900

    20.12.10 17:00

---
+++ workspace/C_Algorithm/Algo_2020_12/Algo12_Week_2/Main_20303_할로윈의양아치.java
@@ -0,0 +1,27 @@
+package Algo12_Week_2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Main_20303_할로윈의양아치 {
+	static int N, M, K;
+	static LinkedList<Point>[] list;
+
+	public static class Point {
+		int from, to;
+
+		public Point(int from, int to) {
+			this.from = from;
+			this.to = to;
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+	}
+}
