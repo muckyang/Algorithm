bea1dd7c4d4aa29a76e7fc188656fe1353a94017
Kim Young Nam <yn782@naver.com>
2020-12-25 22:57:19 +0900

    23:00

--- workspace/D_JAVA_Example/src/Set/ExTreeSet.java
+++ workspace/D_JAVA_Example/src/Set/ExTreeSet.java
@@ -2,7 +2,7 @@ package Set;
 
 import java.util.HashSet;
 
-public class ExHashSet {
+public class ExTreeSet {
 	public static void main(String[] args) {
 		HashSet<Integer> hs = new HashSet<>();
 		
