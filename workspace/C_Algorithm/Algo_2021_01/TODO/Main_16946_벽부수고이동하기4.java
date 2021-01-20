754392ba4d9be9e8eb280d6e8af61ac12771c5a3
Kim Young Nam <yn782@naver.com>
2021-01-20 20:47:43 +0900

    21.01.20 20:47

--- workspace/C_Algorithm/Algo_2021_01/TODO/Main_16946_벽부수고이동하기4.java
+++ workspace/C_Algorithm/Algo_2021_01/TODO/Main_16946_벽부수고이동하기4.java
@@ -1,4 +1,4 @@
-package Algo01_Week3;
+package TODO;
 
 import java.io.BufferedReader;
 import java.io.IOException;
---
+++ workspace/C_Algorithm/Algo_2021_01/TODO/Solution_외벽점검.java
@@ -0,0 +1,41 @@
+package TODO;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class Solution_외벽점검 {
+
+	static int res;
+	static boolean wall[];
+	static boolean[] temp;
+
+	public static void main(String[] args) {
+		int[] answer = { 1, 2, 3, 4, 5 };
+		int[] res = solution(answer);
+		System.out.println();
+	}
+
+	public static int[] solution(int[] answer) {
+		int[] res = null;
+		int[] su = { 5, 8, 10 };
+		int[][] student = { { 1, 2, 3, 4, 5 }, { 2, 1, 2, 3, 2, 4, 2, 5 }, { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 } };
+		int[] cnt = new int[3];
+		LinkedList<Integer> list=new LinkedList<>();
+		int max = 0;
+		for (int i = 0; i < answer.length; i++) {
+			for (int j = 0; j < 3; j++) {
+				int dap = i % su[j];
+				if (answer[i] == student[j][dap]) {
+					cnt[j]++;
+					max = Math.max(cnt[j], max);
+				}
+			}
+		}
+		for (int i = 0; i < 3; i++) {
+			if (max == cnt[i])
+				list.add(i);
+		}
+		return res;
+	}
+
+}
