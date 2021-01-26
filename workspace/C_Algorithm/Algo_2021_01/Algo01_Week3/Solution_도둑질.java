16993aea02938726601554c1f6ee8d28dffdadb4
Kim Young Nam <yn782@naver.com>
2021-01-26 18:05:15 +0900

    21.01.26 18:00

--- workspace/C_Algorithm/Algo_2021_01/Algo01_Week3/Solution_도둑질.java
+++ workspace/C_Algorithm/Algo_2021_01/Algo01_Week3/Solution_도둑질.java
@@ -3,36 +3,33 @@ package Algo01_Week3;
 public class Solution_도둑질 {
 
 	public static void main(String[] args) {
-		int[] money = { 1, 2, 3, 1,5};
+		int[] money = { 1, 2, 3, 1, 5 };
 		System.out.println(solution(money));
 	}
 
-	static int cache[][];
+	static int cache1[][];
+	static int cache2[][];
 	static int don[];
 
 	public static int solution(int[] money) {
 		int res = 0;
 		don = money.clone();
-		cache = new int[2][money.length];
-		res = solve(0, 0, money[0]);// 0 선택
-		res =Math.max(res, solve(1, 1, money[1]));// 1선택
+		cache1= new int[3][money.length];/// 0번째 선택여부 , 점프 수
+		cache2= new int[3][money.length];/// 0번째 선택여부 , 점프 수
+		res = solve1(0, 0, 0);// 0 선택
 		return res;
 	}
 
-	private static int solve(int st, int num, int sum) {
-		if (num >= don.length)
-			return sum;
-		if (st == 0 && num == don.length - 1)
-			return 0;
-		if (cache[st][num] != 0)
-			return cache[st][num];
-		int max = sum;
-		
-		if (num + 2 < don.length)
-			max = Math.max(max, solve(st, num + 2, sum + don[num + 2]));
-		if (num + 3 < don.length)
-			max = Math.max(max, solve(st, num + 3, sum + don[num + 3]));
-		return cache[st][num] = max;
+	private static int solve1(int last, int jump, int sum) {
+		int res = 0;
+
+//		if (now < 0) {// 처음
+//			solve(0, now + 2, sum + don[0]);
+//			solve(0, now + 3, sum + don[0]);
+//		}
+		// now 건너뛰기
+	
+		return res;
 	}
 
 }
---
+++ workspace/C_Algorithm/Algo_2021_01/Algo01_Week4/Main_14725_개미굴TODO.java
@@ -0,0 +1,5 @@
+package Algo01_Week4;
+//Trie문제 
+public class Main_14725_개미굴TODO {
+
+}
